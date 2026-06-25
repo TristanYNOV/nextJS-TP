b0ebf7605302ba19c0e383acadfaeaac1974a81d
TristanEPSI <tristan.gottschak@laposte.net>
2026-06-25 16:05:34 +0200

    update: style

--- src/app/profil/_components/historicPost.tsx
+++ src/app/profil/_components/historicPost.tsx
@@ -1,17 +1,31 @@
 "use client";
-import JobPost from "@/components/job/jobPost";
 import {useApplicationHistoricStore} from "@/store/jobsApplicationHistoric";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import UnfoldMoreIcon from "@mui/icons-material/UnfoldMore";
 
 export default function HistoricPost() {
     const historicPosts = useApplicationHistoricStore((state) => state.applications);
 
-    console.log(historicPosts);
     return ( historicPosts.length > 0 ?
-            <div className="h-[45%] overflow-y-auto flex flex-col gap-2">
+            <section className="h-[45%] overflow-y-auto flex flex-col gap-8">
                 {historicPosts.map(doc => (
-                    <JobPost job={doc} key={doc.id}/>
+                    <article className="flex flex-col gap-2" key={doc.id}>
+                        <div className="flex gap-2 items-center primary-text">
+                            <CalendarMonthIcon/>
+                            <span>{doc.data.job_date}</span>
+                        </div>
+                        <h3 className="text-xl font-bold">{doc.data.title}</h3>
+                        <div className="flex items-center gap-3 primary-text">
+                            <UnfoldMoreIcon className="rotate-90 text-xs"/>
+                            {doc.tags.map(tag => (<span className="text-xs">{tag}</span>))}
+                        </div>
+                        <div>
+                            {doc.data.job_description}
+                        </div>
+                        <div className="w-full border border-[var(--primary-color)]"></div>
+                    </article>
                 ))}
-            </div> :
+            </section> :
             <div className="ml-4">Veuillez sauvegarder au moins une annonce.</div>
     )
 }
--- src/app/profil/page.tsx
+++ src/app/profil/page.tsx
@@ -11,15 +11,15 @@ export default async function ProfilPage() {
 
     return (
         <div className="p-12">
-            <h1 className="font-bold text-4xl mb-8 underline">{title}</h1>
+            <h1 className="font-bold mb-8 text-4xl underline">{title}</h1>
             <main className="h-full overflow-y-auto flex flex-col gap-8">
                 <div className="flex flex-col gap-2">
-                    <h2 className="primary-text text-xl mb-2">{pageInfo.subtitle_1}</h2>
+                    <h2 className="primary-text mb-2 text-[1.75rem]">{pageInfo.subtitle_1}</h2>
                     <SavedArticles existingPosts={allPost}/>
                 </div>
 
-                <div className="flex flex-col gap-2">
-                    <h2 className="primary-text text-xl mb-2">{pageInfo.subtitle_2}</h2>
+                <div className="flex flex-col gap-8">
+                    <h2 className="primary-text text-[1.75rem]">{pageInfo.subtitle_2}</h2>
                     <HistoricPost/>
                 </div>
             </main>

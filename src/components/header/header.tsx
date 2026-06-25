017adf8ea08067953b4b7ae9396f73c606c8042b
TristanEPSI <tristan.gottschak@laposte.net>
2026-06-25 16:28:25 +0200

    style: better logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,8 +5,8 @@ export default async function Header() {
     return(
         <header className="bg-secondary light-text flex justify-between items-center px-4 h-12">
             <Link href={'/'} className="ml-4 text-xs">
-                <div className="flex gap-2">
-                    <div className="rotate-90">{'{]'}</div>
+                <div className="flex items-center gap-2 text-xl">
+                    <div className="rotate-90">{'{ ]'}</div>
                     <span>D E V</span>
                 </div>
             </Link>
